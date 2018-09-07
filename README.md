b88ca0b7929a88b2759909d7c9188f1e26158f43
yangjun@chinafintech.cn <yangjun@chinafintech.cn>
2018-09-07 18:32:41 +0800

    s

---
+++ README.md
@@ -0,0 +1,74 @@
+# umi-plugin-locale-yaml
+
+`umi`多语言控制插件YAML版本。
+
+基于[umi-plugin-locale](https://github.com/umijs/umi/tree/master/packages/umi-plugin-locale)实现。
+
+## 配置
+
+**.umirc.js**
+
+```js
+export default {
+  plugins: [
+    [
+      'umi-plugin-locale-yaml',
+      {
+        locale: {
+          default: 'zh-CN', //默认语言 zh-CN
+          baseNavigator: true, // 为true时，用navigator.language的值作为默认语言
+          antd: true // 是否启用antd的<LocaleProvider />
+        }
+      }
+    ]
+  ]
+};
+```
+
+## 目录及约定
+
+```
+.
+├── dist/                          
+├── mock/                         
+└── src/                          
+    ├── layouts/index.js          
+    ├── pages/                    
+    └── locales               // 多语言文件存放目录，里面的文件会被umi自动读取
+        ├── zh-CN.yml
+        └── en-US.yml
+├── .umirc.js                     
+├── .env                          
+└── package.json
+```
+
+
+>如果`.umirc.js`里配置了`singular: true`，`locales`要改成`locale`
+
+
+## 多语言文件约定
+
+多语言文件的命名规范：`<lang>-<COUNTRY>.yml`
+
+
+多语言文件的内容规范：键-值组成的字面量，如下：
+
+zh-CN.yml
+
+```javascript
+App:
+  appName: 国际化
+  version: 版本
+  copyright: C
+```
+
+en-US.yml
+
+```javascript
+App:
+  appName: i18n
+  version: Ver
+  copyright: C
+```
+
+## 使用方法同 umi-plugin-locale  仅修改了读取的配置文件格式
---
+++ src/index.js
@@ -0,0 +1,84 @@
+import { join, dirname } from 'path';
+import {
+  existsSync,
+  statSync,
+  readdirSync,
+  readFileSync,
+  writeFileSync,
+} from 'fs';
+import { winPath } from 'umi-utils';
+import Mustache from 'mustache';
+import yamlReader from 'yaml-reader';
+import flat from 'flat';
+
+// export for test
+export function getLocaleFileList(absSrcPath, singular) {
+  const localeList = [];
+  const localePath = join(absSrcPath, singular ? 'locale' : 'locales');
+  if (existsSync(localePath)) {
+    const localePaths = readdirSync(localePath);
+    for (let i = 0; i < localePaths.length; i++) {
+      const fullname = join(localePath, localePaths[i]);
+      const stats = statSync(fullname);
+      const fileInfo = /^([a-z]{2})-([A-Z]{2})\.(yml|yaml)$/.exec(localePaths[i]);
+      if (stats.isFile() && fileInfo) {
+          const messages = flat.flatten(yamlReader.read(winPath(fullname)));
+          console.log(messages)
+          process.exit(0)
+        localeList.push({
+          lang: fileInfo[1],
+          country: fileInfo[2],
+          name: `${fileInfo[1]}-${fileInfo[2]}`,
+          messages: JSON.stringify(messages),
+        });
+      }
+    }
+  }
+  return localeList;
+}
+
+export default function(api, options = {}) {
+  const { config, paths } = api;
+
+  api.addPageWatcher(
+    join(paths.absSrcPath, config.singular ? 'locale' : 'locales'),
+  );
+
+  api.onOptionChange(newOpts => {
+    options = newOpts;
+    api.rebuildTmpFiles();
+  });
+
+  api.addRendererWrapperWithComponent(() => {
+    const localeFileList = getLocaleFileList(paths.absSrcPath, config.singular);
+    const wrapperTpl = readFileSync(
+      join(__dirname, '../template/wrapper.jsx.tpl'),
+      'utf-8',
+    );
+    const defaultLocale = options.default || 'zh-CN';
+    const wrapperContent = Mustache.render(wrapperTpl, {
+      localeList: localeFileList,
+      antd: options.antd === undefined ? true : options.antd,
+      baseNavigator:
+        options.baseNavigator === undefined ? true : options.baseNavigator,
+      useLocalStorage: true,
+      defaultLocale,
+      defaultLang: defaultLocale.split('-')[0],
+      defaultAntdLocale: defaultLocale.replace('-', '_'),
+    });
+    const wrapperPath = join(paths.absTmpDirPath, './LocaleWrapper.jsx');
+    writeFileSync(wrapperPath, wrapperContent, 'utf-8');
+    return wrapperPath;
+  });
+
+  api.modifyAFWebpackOpts(memo => {
+    return {
+      ...memo,
+      alias: {
+        ...(memo.alias || {}),
+        'umi/locale': join(__dirname, './locale.js'),
+        'react-intl': dirname(require.resolve('react-intl/package.json')),
+      },
+    };
+  });
+}
